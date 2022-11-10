0ea1ce2ff955f3cee77e28504d35a10572e1b41c
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-11-10 23:34:11 +0100

    Added account handler

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 #include <vector>
 #include <string>
 #include <cstdlib>
+#include <Windows.h>
 #include "account.h"
+#include "launcher.h"
 
 using namespace std;
 
@@ -25,7 +27,9 @@ vector<string> split(string s, char c) {
 }
 
 int main(int argc, char **argv) {
-    int i;
+    int i, choice;
+    string line;
+    vector<Account> accounts;
 
     ifstream csv("./accounts.csv");
 
@@ -34,18 +38,67 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    string line;
-    vector<Account> accounts;
-
     while(getline(csv, line)) {
         vector<string> data = split(line, ',');
 
         if(data.size() == 3) {
-            Account account(data[0], data[1], data[2]);
+            Account account(data[1], data[2], data[0]);
             accounts.push_back(account);
         } else
             cout << "Error: invalid account data" << endl;
     }
 
+    cout << "Choose an account number:" << endl;
+
+    for(i = 0; i < accounts.size(); i++)
+        cout << i << ")\t" << accounts[i].getId() << endl;
+
+    cin >> choice;
+
+    while(choice < 0 || choice > accounts.size() - 1) {
+        cout << "Error: invalid entry" << accounts.size() << endl;
+        cout << "Choose an account number:" << endl;
+        cin >> choice;
+    }
+
+
+
+
+
+
+
+
+    Launcher L;
+    L.disconnect();
+
+    Sleep(7500);
+
+    HWND test = FindWindowA(NULL, "Riot Client Main");
+    SetForegroundWindow(test);
+
+    RECT rect;
+    HWND phwnd;
+
+    GetWindowRect(test, &rect);
+    phwnd = GetForegroundWindow();
+
+    for(i = 0; i < accounts[choice].getId().size(); i++)
+        keyboardWriting(accounts[choice].getId()[i]);
+
+    Sleep(250);
+
+    leftClick(rect.left + 170, rect.top + 325);
+
+    Sleep(50);
+
+    for(i = 0; i < accounts[choice].getPassword().size(); i++)
+        keyboardWriting(accounts[choice].getPassword()[i]);
+
+    Sleep(250);
+    leftClick(rect.left + 170, rect.top + 430); // Stay signed in
+    leftClick(rect.left + 170, rect.top + 700); // Connect button
+    Sleep(3300);
+
+    leftClick(rect.left + 170, rect.top + 800);
     return 0;
 }
